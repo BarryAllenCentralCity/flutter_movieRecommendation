becfeeecfd7d7574e5e11314926a26dabe56e4b8
BarryAllenCentralCity <93136153+BarryAllenCentralCity@users.noreply.github.com>
2023-03-30 13:16:59 +0530

    main

--- lib/Features/movie_flow/landing/landing_screen.dart
+++ lib/Features/movie_flow/landing/landing_screen.dart
@@ -17,7 +17,7 @@ class LandingScreen extends StatelessWidget {
           children: [
             Text("Let's find a movie", style: Theme.of(context).textTheme.headline5,textAlign: TextAlign.center,),
             const Spacer(),
-            Image.network('https://media.istockphoto.com/id/638625882/vector/teen-in-cinema.jpg?s=612x612&w=0&k=20&c=kQOnh8GGkeL23O0UVFBnBIxZshh-VSzkf1TKjVvFLE8='),
+            Image.asset('images/image01.png'),
             const Spacer(),
             PrimaryButton(onPressed: nextPage, text: 'Get Started', width: 120.0,),
             const SizedBox(height: kMediumSpacing,)
